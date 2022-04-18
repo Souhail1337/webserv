a76041614f58039fc91f9671b8b54aac8357cd80
Souhail El fchtali <sel-fcht@e2r1p6.1337.ma>
2022-04-18 02:00:02 +0000

    config file kmel

--- config/config.cpp
+++ config/config.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 23:58:18 by sel-fcht          #+#    #+#             */
-/*   Updated: 2022/04/18 00:03:27 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2022/04/18 01:54:11 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,30 @@ void Config::initialize(int i)
 {
     srv[i].name = "";
     srv[i].host = NULL;
+    srv[i].root = NULL;
     srv[i].port = -1;
     srv[i].bodySize = -1;
+    srv[i].error_page = "NULL";
 }
 
+void Config::Linit(location *loc)
+{
+    loc->path = "NULL";
+    loc->method = "NULL";
+    loc->root = " NULL";
+    loc->autoind = false;
+    loc->bodySize = "-1";
+    loc->cgi_path = "NULL";
+    loc->extension = "NULL";   
+}
+void Config::start_parse(std::string input)
+{
+    std::ifstream file(input);
+    if (file < 0)
+        return ("File couldnt open",1);
+    else
+    {
+        
+    }
+}
 
--- config/config.hpp
+++ config/config.hpp
@@ -6,13 +6,18 @@
 /*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 23:38:57 by sel-fcht          #+#    #+#             */
-/*   Updated: 2022/04/18 01:02:48 by sel-fcht         ###   ########.fr       */
+/*   Updated: 2022/04/18 01:57:09 by sel-fcht         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
-
+#include <map>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 class Config
 {
@@ -25,17 +30,30 @@ class Config
         {
             std::string name;
             std::string host;
+            std::string root;
             int port;
             int bodySize;
+            std::string error_page;
             std::vector<location> locations;
         };
         struct location
         {
-            std::string 
+            std::string path;
+            std::string method;
+            std::string root;
+            bool autoind;
+            int bodySize;
+            std::string cgi_path;
+            std::string extension;
+            // some of those are optional but still mzyan tziddhum ..
         };
+
+        
         std::vector<server> srv;
         //start parsing had zbi;
-        void initialize(int i);        
+        void initialize(int i);  
+        void Linit(location *loc);      
+        void start_parse(std::string inputfile);
         
 };
 
