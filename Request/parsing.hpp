b1fcf4dd425257414d57d91c1fe3d479cebf1d70
Souhail El fchtali <sel-fcht@e2r1p6.1337.ma>
2022-04-29 06:47:59 +0000

    parsing request

---
+++ Request/parsing.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-fcht <sel-fcht@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/29 05:23:21 by sel-fcht          #+#    #+#             */
+/*   Updated: 2022/04/29 05:24:37 by sel-fcht         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_HPP
+#define PARSING_HPP
+
+#include <iostream>
+#include <string>
+#include "../config/config.hpp"
+#include <list>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <iterator>
+
+
+
+class parsing
+{
+    private:
+    public:
+        struct request
+        {
+            std::string method;
+            std::string path;
+            std::string 
+        };
+};
+
+#endif
