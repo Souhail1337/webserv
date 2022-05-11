14fbb7aeb4d5ee0350d840ec9773c9c719e257a3
Souhail El fchtali <sel-fcht@e2r1p6.1337.ma>
2022-05-11 18:11:53 +0100

    codes

--- srcs/responses.hpp
+++ srcs/responses.hpp
@@ -12,34 +12,125 @@ class Responses
     public:
         static int ok()
         {
-            return 201;
+            return 200;
         }
         static int CREATED()
         {
-            return 202;
+            return 201;
         }
         static int ACCEPTED()
         {
-            return 203;
+            return 202;
         }
         static int NOT_AUTHORIZED()
         {
-            return 204;
+            return 203;
         }
         static int NO_CONTENT()
         {
-            return 205;
+            return 204;
         }        
         static int RESET_CONTENT()
         {
-            return 206;
+            return 205;
         }
         static int PARTIAL_CONTENT()
         {
-            return 207;
+            return 206;
         }
-            
-
+        static int CONTINUE()
+        {
+            return 100;
+        }
+        static int SWITCHING_PROTOCOLS()
+        {
+            return 101;
+        }
+        static int EARLY_HINTS()
+        {
+            return 103;
+        }
+        static int MULTIPLE()
+        {
+            return 300;
+        }    
+        static int MOVED()
+        {
+            return 301;
+        }
+        static int FOUND()
+        {
+            return 302;
+        }
+        static int SEE_OTHER()
+        {
+            return 303;
+        }
+        static int NOT_MODIFIED()
+        {
+            return 304;
+        }
+        static int BAD_REQUEST()
+        {
+            return 400;
+        }
+        static int UNAUTHORIZED()
+        {
+            return 401;
+        }
+        static int PAYMENT_METHOD()
+        {
+            return 402;
+        }
+        static int FORBIDEN()
+        {
+            return 403;
+        }
+        static int NOT_FOUND()
+        {
+            return 404;
+        }
+        static int METHOD_NOT_ALLOWED()
+        {
+            return 405;
+        }
+        static int NOT_ACCEPTABLE()
+        {
+            return 406;
+        }
+        static int PROXY_AUTHENTIFICATION_REQUIRED()
+        {
+            return 407;
+        }
+        static int REQUEST_TIMEOUT()
+        {
+            return 408;
+        }
+        static int CONFLICT()
+        {
+            return 409;
+        }
+        static int GONE()
+        {
+            return 410;
+        }
+        static int LENGHT_REQUIRED()
+        {
+            return 411;
+        }
+        static int PAYLOAD_TOO_LARGE()
+        {
+            return 413;
+        }
+        static int URI_TOO_LONG()
+        {
+            return 414;
+        }
+        static int UNSUPPORTED()
+        {
+            return 415;
+        }
+        static int 
 };
 
 
